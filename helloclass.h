32489506b219d947e0fb9645e66a1ad468998f2d
pangxiaox <pangxiaox@example.com>
2018-06-21 14:46:21 +0800

    class

---
+++ helloclass.h
@@ -0,0 +1,15 @@
+#ifndef HELLOCLASS_H
+#define HELLOCLASS_H
+
+#include<iostream>
+using namespace std;
+
+class HelloClass
+{
+      public:
+         HelloClass();
+        ~HelloClass();
+        void sayhello();
+};
+
+#endif
